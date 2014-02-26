ab30128f9358380f76b8ef29d664698294f54aca
oldgeezr <ramstadevensen@hotmail.com>
2014-02-26 14:36:38 +0100

    Fixed UDP Broadcast for POSIX

--- Prototyping/UDP_Broadcast_Client/main.go
+++ Prototyping/UDP_Broadcast_Client/main.go
@@ -8,21 +8,22 @@ import (
 func runClient(udpAddr net.UDPAddr) {
     socket, err := net.DialUDP("udp4", nil, &udpAddr)
     if err != nil {
-        fmt.Println("Error: ", err.Error())
+        fmt.Println("Error_1: ", err.Error())
         return
     }
 
     data := []byte("I cannot allow you to do that Dave.")
-    n, err := socket.WriteToUDP(data, &udpAddr)
-
+    //n, err := socket.WriteToUDP(data, &udpAddr)
+    n, err := socket.Write(data)
     if err != nil {
-        fmt.Println("Error: ", err.Error())
+        fmt.Println("Error_2: ", err.Error())
     }
-    fmt.Println("Sent ", string(n), " bytes")
+    fmt.Println("Sent ", n, " bytes")
     return
 }
 
 func main() {
+
     ipv4_broadcast := net.IPv4(255, 255, 255, 255)
     udpAddr := net.UDPAddr{IP : ipv4_broadcast, Port: 12345}
 
