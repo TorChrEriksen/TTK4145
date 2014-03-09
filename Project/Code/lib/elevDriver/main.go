dd310cf9f678209b1d7693a100f0ec075897f1f0
msadowski <msadowski90@gmail.com>
2014-03-09 14:32:56 +0100

    switched to my branch. Working elevator driver module

--- Project/Code/lib/elevDriver/main.go
+++ Project/Code/lib/elevDriver/main.go
@@ -6,20 +6,41 @@ package main
 
 import(
     "./lib"
-    //"fmt"
+    "fmt"
     //"time"
     //"os"
 )
 
+/*Driver interface Signals from controller:
+STOP = 10
+INT_BTN_1 = 11
+INT_BTN_2 = 12
+INT_BTN_3 = 13
+INT_BTN_4 = 14
+EXT_BTN_1_UP = 21
+EXT_BTN_2_UP = 22
+EXT_BTN_2_DOWN = 23
+EXT_BTN_3_UP = 24
+EXT_BTN_3_DOWN = 25
+EXT_BTN_4_DOWN = 26
+FLOOR_NO = 30
+FLOOR_1 = 31
+FLOOR_2 = 32
+FLOOR_3 = 33
+FLOOR_4 = 34
+*/
+
 func main(){
    driverInterface.Init()
    //driverInterface.GetFloorSignal()
-   driverInterface.Test_Run()
+   //driverInterface.Test_Run()
 	// Create driver with all the channels we need
-	//buttonChannel := make(chan int)
-	//stopChannel := make(chan int)
-	//driverInterface.Create(buttonChannel)
-	//stopChannel := make(chan int)
+	intButtonChannel := make(chan int)
+    extButtonChannel := make(chan int)
+    floorChannel := make(chan int)
+    stopChannel := make(chan int)
+//intBtChan chan int, floorChan chan int, stopChan chan int, extBtChan chan int
+	driverInterface.Create(intButtonChannel, floorChannel, stopChannel, extButtonChannel)
 	
 	//driverInterface.SetSpeed(-300)
 	
@@ -29,22 +50,26 @@ func main(){
 	//driverInterface.SetDoorLamp(1)    //OK!
 	//driverInterface.SetButtonLamp("DOWN",2,1) //OK! (Panic mode also implemented)
 
-	/*
 	for {
 		select {
-			case buttonSignal := <-buttonChannel :
+			case intButtonSignal := <- intButtonChannel :
 				// release thread (use a channel or fire a go routine)
 				go func() {
-					fmt.Println(buttonSignal)
-					time.Sleep(time.Second * 2) // Just for demonstration why we want to release the thread!
+					fmt.Println(intButtonSignal)
+					//time.Sleep(time.Second * 2) // Just for demonstration why we want to release the thread!
 				}()
-			case stopSignal := <-stopChannel :
+            case floorSignal := <- floorChannel :
+                go func() {
+					fmt.Println(floorSignal)
+                }()
+			case stopSignal := <- stopChannel :
 			   go func(){
-			      fmt.Println(stopSignal)
+			        fmt.Println(stopSignal)
 			   }()
-
-			// more cases
-//			case floorSignal := <-floorChannel :
+            case extButtonSignal := <- extButtonChannel :
+                go func() {
+                    fmt.Println(extButtonSignal)
+                }()
 		}
-	}*/
+	}
 }
