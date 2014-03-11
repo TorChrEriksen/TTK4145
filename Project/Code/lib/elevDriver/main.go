7d5bcdce481aa37f1a1d597d25c8111de5750af2
McHaul <fredriu@stud.ntnu.no>
2014-03-11 02:17:01 +0100

    Update main.go
    *Added func cost(...)
    *Described communication (very loosely)
    *Fixed some obvious indexing errors
    *Modified runOffList(...) to be more correct and to get orders from channels instead of shared array.
    *Added func contains(...)
    and some other stuff...
    
    I realize this is not only not running but extremely messy. But everything on the Running should now be in here. There are some choices to be made to where to put the functions, as well as how many we want defined rather than an local function etc. etc. but if i'm not mistaken this is for the big piecing together rather than something we get perfect from the getgo.
    
    Things took longer than expected today (shared array vs. channel f.exa.) so I didn't have the time to test in the lab. But as I said, the most important pieces should be there.

--- Project/Code/lib/elevDriver/main.go
+++ Project/Code/lib/elevDriver/main.go
@@ -18,12 +18,12 @@ INT_BTN_1 = 11
 INT_BTN_2 = 12
 INT_BTN_3 = 13
 INT_BTN_4 = 14
-EXT_BTN_1_UP = 21
-EXT_BTN_2_UP = 22
-EXT_BTN_2_DOWN = 23
-EXT_BTN_3_UP = 24
-EXT_BTN_3_DOWN = 25
-EXT_BTN_4_DOWN = 26
+EXT_BTN_1_UP = 41
+EXT_BTN_2_UP = 51
+EXT_BTN_2_DOWN = 50
+EXT_BTN_3_UP = 61
+EXT_BTN_3_DOWN = 60
+EXT_BTN_4_DOWN = 70
 FLOOR_NO = 30
 FLOOR_1 = 31
 FLOOR_2 = 32
@@ -54,71 +54,118 @@ Person on 2nd floor wants to go to 4th. The elevator will now go to 2nd, talking
 
 Although this can be fixed with ordersorting
 */
-//Må handle med global orderliste, hva skjer med afterOrders. tenk.
-func runOffList(currentFloor int, orders []int){
+//Må handle med global orderliste, hva skjer med afterOrders. tenk. // Added another for loop that should execute the false additions.
+
+func contains(s []int, e int) bool {
+    for _, a := range s { if a == e { return true } }
+    return false
+}
+
+func runOffList(ordersIn chan int, currentFloor chan int){
 	var maks int
 	var next int
 	var dir string 			//"DOWN" || "UP"
+	var orders []int
 	var afterOrders []int 	// For orders that does not conform (internals in opposite direction)
+	var last_floor int
 
-	for len(orders)==0{
-		driverInterface.SetSpeed(0)
-		time.Sleep(100 * time.Millisecond)
+	go func (){
+		ny := <- ordersIn
+		orders = append(orders, ny - 10)
+		sort.Ints(orders)
 	}
-	
-		//get highest order
-	maks = orders[len(orders)-1]
-	
-		//Get the direction of order and go there
-	if maks < currentFloor {
-		for len(orders)>0{
-			dir = "DOWN"
-			go func(){
+
+	go func (){
+		new_floor := <- currentFloor
+		last_floor = new_floor - 30
+
+	}
+
+	go func (){
+		for {
+			if len(orders)!=0{
+
 				maks = orders[len(orders)-1]
-				if maks>currentFloor{
-					afterOrders, orders = append(afterOrders, maks), orders[:len(orders)-1]
+
+				if maks< last_floor{
+					for len(orders)>0{
+						dir = "DOWN"
+
+						go func(){
+							maks = orders[len(orders)-1]
+							if maks>last_floor{
+								afterOrders, orders = append(afterOrders, maks), orders[:len(orders)-1]
+								sort.Ints(afterOrders)
+							}else{
+								next=orders[len(orders)-1]
+							}
+						}()
+						go func(){
+							drive(dir)
+							for last_floor!=next{
+								time.Sleep(10 * time.Millisecond)
+							}
+							drive("STOP")
+							orders = orders[:len(orders)-1]
+							drive("OPEN")
+						}()
+					}
 				}else{
-					next = orders[len(orders)-1]
-				}
-			}()
+					for len(orders)>0{
+						dir = "UP"
+						go func(){
+							minn = orders[0]
+							if minn<last_floor{
+								afterOrders, orders = append(afterOrders, minn), orders[1:]
+								sort.Ints(afterOrders)
+							}else{
+								next = orders[0]
+							}
+						}()
 
-			go func(){
-				drive(dir)
-				for currentFloor!=next{
-					time.Sleep(10 * time.Millisecond)
+						go func(){
+							drive(dir)
+							for last_floor!=next{
+								time.Sleep(10 * time.Millisecond)
+							}
+							drive("STOP")
+							orders = orders[1:]
+							drive("OPEN")
+						}()
+					}
 				}
-				drive("STOP")
-				orders = orders[:len(orders)-1]
-				drive("OPEN")
-			}()
-		}orders, afterOrders = afterOrders, nil
-
-
-	}else{
-		for len(orders)>0{
-			dir = "UP"
-			go func(){
-				minn = orders[0]
-				if minn<currentFloor{
-					afterOrders, orders = append(afterOrders, minn), orders[0:]
-				}else{
-					next = orders[0]
-				}
-			}()
-
-			go func(){
-				drive(dir)
-				for currentFloor!=next{
-				time.Sleep(10 * time.Millisecond)
-				}
-				drive("STOP")
-				orders = orders[0:]
-				drive("OPEN")
-			}()
+				orders, afterOrders = afterOrders, nil
+			}
 		}
-		orders, afterOrders = afterOrders, nil
 	}
 }
+
+
+//func(distance to order + number of orders on the way) think about adding +len(afterOrderList[sort.Search(afterOrderList, new_order)]) to some
+func cost(orderList []int, afterOrderList int, currPos int, dir_now int, new_order int, new_order_dir int) int{
+	select {
+		case dir_now==1 && new_order_dir==1:
+			if currPos<new_order{
+				return math.Sqrt( (new_order-currPos)+len(orderList[:sort.Search(orderList, new_order)]))
+			}else{
+				return math.Sqrt( ((2*orderList[len(orderList)-1])-new_order-currPos)+len(orderList))
+			}
+
+		case dir_now==0 && new_order_dir==0:
+			if new_order<currPos{
+				return math.Sqrt((currPos-new_order)+len(orderList[sort.Search(orderList, new_order):]))
+			}else{
+				return math.Sqrt(new_order+currPos-orderList[0]+len(orderList))
+			}
+
+		case dir_now==1&&new_order_dir==0:
+			return math.Sqrt(2*orderList[len(orderList)-1]-currPos-new_order+len(orderList))
+
+		case dir_now==0&&new_order_dir==1:
+			return math.Sqrt(currPos+new_order-orderList[0]+len(orderList))
+	}
+}
+
 
 
 func main(){
@@ -134,11 +181,13 @@ func main(){
 	driverInterface.Create(intButtonChannel, floorChannel, stopChannel, extButtonChannel, timeoutChannel)
 	
 
+
 	//VARIABLES
 	//last known floor
 	var last_floor int
 	//orderlist
-	var orders []int
+	ordersIn := make(chan int)
+	currentFloor := make(chan int)
 	var speed int
 	var dir string //DOWN || UP
 
@@ -151,25 +200,21 @@ func main(){
 	//driverInterface.SetDoorLamp(1)    //OK!
 	//driverInterface.SetButtonLamp("DOWN",2,1) //OK! (Panic mode also implemented)
 
-	//runOffList(last_Floor, orders)
+	//runOffList(ordersIn, currentFloor)
 
 	for {
 		select {
 			case intButtonSignal := <- intButtonChannel :
 				// release thread (use a channel or fire a go routine)
 				go func() {
-					orders = append(orders, intButtonSignal)
+					ordersIn <- intButtonSignal
 					//can use Sort.IsSorted to check if need sorting
-					Sort.Ints(orders)
-					fmt.Println(orders)
 					//time.Sleep(time.Second * 2) // Just for demonstration why we want to release the thread!
 				}()
             case floorSignal := <- floorChannel :
                 go func() {
-					last_floor = floorSignal - 30
-					fmt.Println(a)
-					
-                	}()
+                	currentFloor <- floorSignal					
+               	}()
 			case stopSignal := <- stopChannel :
 			   go func(){
 			        fmt.Println(stopSignal)
@@ -181,17 +226,25 @@ func main(){
             case extButtonSignal := <- extButtonChannel :
                 go func() {
                 	//TODO: Change the value from pressing the ext. buttons
-                	//to make simpler to sort the signals, a%2==0:UP
-			        if extButtonSignal%2==0{
+                	//to make simpler to sort the signals, a%2==1:UP
+                	
+                	// lowest[cost , elevatorID]
+                	var lowest [2]int
 
-			        	}else{
+                	// incomming[floor , direction]
+                	var incomming  := [2]int{((extButtonChannel-extButtonChannel%2)-30)/10, extButtonChannel%2}	
 
-			        	}
-			        /*If in same direction as other orders:
-			        	if on a floor in orders: forget
-			        	else: get cost from others, send orders to lowest
-			        */
-                    fmt.Println(extButtonSignal)
+                	/*
+					if 
+                	*/
+
+                	/*
+					lowest = [thisElev.cost , thisElev.ID]
+					get the cost from the others
+						if Other.Cost < lowest[0]:
+							lowest = [Other.cost, Other.ID]
+					send incomming[0] to Other.ID's OrderCHANL
+                	*/
                 }()
             case floorTimeout := <- timeoutChannel :
                 go func() {
@@ -200,3 +253,69 @@ func main(){
 		}
 	}
 }
+
+
+
+
+	/*
+	for len(orders)==0{
+		driverInterface.SetSpeed(0)
+		time.Sleep(100 * time.Millisecond)
+	}
+	
+		//get highest order
+
+	maks = orders[len(orders)-1]
+	for len(orders){
+		//Get the direction of order and go there
+		if maks < currentFloor {
+			for len(orders)>0{
+				dir = "DOWN"
+				go func(){
+					maks = orders[len(orders)-1]
+					if maks>currentFloor{
+						afterOrders, orders = append(afterOrders, maks), orders[:len(orders)-1]
+					}else{
+						next = orders[len(orders)-1]
+					}
+				}()
+
+				go func(){
+					drive(dir)
+					for currentFloor!=next{
+						time.Sleep(10 * time.Millisecond)
+					}
+					drive("STOP")
+					orders = orders[:len(orders)-1]
+					drive("OPEN")
+				}()
+
+				go func(){}
+
+		}else{
+			for len(orders)>0{
+				dir = "UP"
+				go func(){
+					minn = orders[0]
+					if minn<currentFloor{
+						afterOrders, orders = append(afterOrders, minn), orders[1:]
+					}else{
+						next = orders[0]
+					}
+				}()
+
+				go func(){
+					drive(dir)
+					for currentFloor!=next{
+					time.Sleep(10 * time.Millisecond)
+					}
+					drive("STOP")
+					orders = orders[1:]
+					drive("OPEN")
+				}()
+			}
+		orders, afterOrders = afterOrders, nil
+		}
+	}
+}
+*/
