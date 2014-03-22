7726d246e042b455740eaad5ee66cb245fb75525
Tor-Christian Eriksen <trofast@gmail.com>
2014-03-22 09:37:46 +0100

    Updates

--- Project/Code/lib/netCtrl/netCtrl.go
+++ Project/Code/lib/netCtrl/netCtrl.go
@@ -241,8 +241,8 @@ func (nc *NetController) validateConnections() {
 
                         nc.al.Send_To_Log(nc.Identifier, logger.INFO, "Attempting to kill tcp connection")
                         tcpClient.KillTCPConnection()
-                        nc.tcpClients = append(nc.tcpClients[:k], nc.tcpClients[k + 1])
-
+                        nc.tcpClients = append(nc.tcpClients[:k], nc.tcpClients[k + 1:])
+                        //nc.tcpClients = append([]SocketClient.SocketClient, nc.tcpClients[:len(nc.tcpClients) - 1])
                     }
                 }
             }
@@ -254,15 +254,21 @@ func (nc *NetController) validateConnections() {
 
                         nc.al.Send_To_Log(nc.Identifier, logger.INFO, "Attempting to kill udp connection")
                         udpClient.KillUDPConnection()
-                        nc.udpClients = append(nc.udpClients[:j], nc.udpClients[j + 1])
+                        nc.udpClients = append(nc.udpClients[:j], nc.udpClients[j + 1:])
+                        //nc.udpClients = append([]SocketClient.SocketClient(nil), nc.udpClients[:len(nc.udpClients) - 1])
                     }
                 }
             }
 
-            nc.clientList = append(nc.clientList[:n], nc.clientList[n + 1])
+            nc.clientList = append(nc.clientList[:n], nc.clientList[n + 1:])
+            //nc.clientList = append([]ClientCtrl.ClientInfo, nc.clientList[:len(nc.clientList) - 1])
         }
     }
 }
+
+//[]SocketClient.SocketClient
+//[]SocketClient.SocketClient
+//[]ClientCtrl.ClientInfo
 
 // Parameter is not to be a string, but serialized data.
 // TODO Waiting for structure.
